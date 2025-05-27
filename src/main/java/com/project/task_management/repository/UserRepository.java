ddba6bdb585dd34790bec91cb57389231147e904
kaytee07 <kofi.taylor07@gmail.com>
2025-05-27 18:32:09 +0000

    feat: add UserRepository to interact with the User table

---
+++ src/main/java/com/project/task_management/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.project.task_management.repository;
+import com.project.task_management.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
