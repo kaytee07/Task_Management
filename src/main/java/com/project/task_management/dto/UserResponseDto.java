345b8d409615f7e5f44922f9fdc6c0d6595711b3
kaytee07 <kofi.taylor07@gmail.com>
2025-05-28 01:09:52 +0000

    feat: add UserResponseDto to transfer data from controller to client

---
+++ src/main/java/com/project/task_management/dto/UserResponseDto.java
@@ -0,0 +1,42 @@
+package com.project.task_management.dto;
+
+import java.util.UUID;
+
+public class UserResponseDto {
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String username;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
